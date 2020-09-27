d3ef6844337b01c89322894bb3874bb4f0a66e43
jongreen-developer <63204002+jongreen-developer@users.noreply.github.com>
2020-09-27 20:19:08 -0230

    Code

--- Lab2/Paycheck.php
+++ Lab2/Paycheck.php
@@ -6,6 +6,10 @@
 <body>
 <?php
 
+$hours = 0;
+$rate = 0;
+$valid = true;
+
 if (isset($_POST['Submit'])) {
 
     if(!empty($_POST['hours'])) {
@@ -14,9 +18,15 @@ if (isset($_POST['Submit'])) {
 
         if (preg_match("/([0-9]|[1-8][0-9]|9[0-9]|1[0-5][0-9]|16[0-8])/", $hours) == 0) {
 
-            echo "Please enter a number between 0 and 168";
+            echo "Please enter the number of weekly hours worked (1 - 168)";
+            $valid = false;
 
         }
+
+    } else {
+
+        echo "Please enter the number of weekly hours worked (ex. 40)";
+        $valid = false;
 
     }
 
@@ -24,11 +34,33 @@ if (isset($_POST['Submit'])) {
 
         $rate = $_POST['rate'];
 
-        if (preg_match("/^\d+(\.\d{1,2})?/", $rate) != 0) {
+        if (preg_match("/\d+(\.\d{1,2})?/", $rate) == 0) {
 
-            echo "Please enter a rate using the format 0.00";
+            echo "Please enter the hourly rate (ex. 15.00)";
+            $valid = false;
 
         }
+
+    } else {
+
+        echo "Please enter the hourly rate (ex. 15.00)";
+        $valid = false;
+
+    }
+
+    if($valid != false){
+
+        if($hours >= 40){
+
+            $pay = (40 * $rate) + (($hours - 40) * ($rate * 1.5));
+
+        } else {
+
+            $pay = (40 * $rate);
+
+        }
+
+        echo "The wages earned are $".$pay;
 
     }
 
